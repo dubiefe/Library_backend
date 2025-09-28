040eff2ebd754afda72122fea02285d17ced9ab9
dubiefe <emiliedubief@free.fr>
2025-09-28 14:00:59 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { readFile, writeFile } from "fs/promises";
+
+const app = express();
+app.use(express.json());
+
+const DATA_FILE = "./data.json";
+
+app.get("/api/data", async (req, res) => {
+  const data = await readFile(DATA_FILE, "utf-8");
+  res.json(JSON.parse(data));
+});
+
+app.post("/api/save", async (req, res) => {
+  await writeFile(DATA_FILE, JSON.stringify(req.body, null, 2));
+  res.json({ success: true });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
