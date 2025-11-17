1c7496f9bbb90b84c4a4c8085c84b4da96471407
dubiefe <emilie.dubief@etu.univ-grenoble-alpes.fr>
2025-11-17 08:45:53 +0100

    Add login data

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ import cors from "cors";
 import { readFile, writeFile } from "fs/promises";
 
 const app = express();
-app.use(cors({ origin: "https://dubiefe.github.io" }));
+app.use(cors({ origin: [
+    "https://dubiefe.github.io",
+    "http://localhost:5173"
+]}));
 app.use(express.json());
 
 const DATA_FILE = "./data.json";
+const LOGIN_DATA_FILE = "./login_data.json";
 
 app.get("/api/data", async (req, res) => {
   const data = await readFile(DATA_FILE, "utf-8");
@@ -15,6 +19,16 @@ app.get("/api/data", async (req, res) => {
 
 app.post("/api/save", async (req, res) => {
   await writeFile(DATA_FILE, JSON.stringify(req.body, null, 2));
+  res.json({ success: true });
+});
+
+app.get("/api/data_login", async (req, res) => {
+  const data = await readFile(LOGIN_DATA_FILE, "utf-8");
+  res.json(JSON.parse(data));
+});
+
+app.post("/api/save_login", async (req, res) => {
+  await writeFile(LOGIN_DATA_FILE, JSON.stringify(req.body, null, 2));
   res.json({ success: true });
 });
 
